b33874c26d75975ca0b892d199d8b4d6e0e9a511
Laura <laurabejaranoa22@gmail.com>
2025-12-11 16:52:40 +0100

    cambios 0.js

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,17 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> at first sight, there is a error because the variable str is being declared twice in the same scope
 
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+// function capitalise(str) {
+// let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//return str;
 
-// =============> write your explanation here
 // =============> write your new code here
+
+function capitalise(str1) {
+  let result = `${str1[0].toUpperCase()}${str1.slice(1)}`;
+  return result;
+}
+console.log(capitalise("laura")); // Output: "Laura"
