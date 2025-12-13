28dd867d56fc993d7834c6de797ecd3f43c69358
Laura <laurabejaranoa22@gmail.com>
2025-12-13 13:24:18 +0100

    answer getLast

--- Sprint-2/2-mandatory-debug/getLastDig.js
+++ Sprint-2/2-mandatory-debug/getLastDig.js
@@ -2,23 +2,36 @@
 
 // Predict the output of the following code:
 // =============> Write your prediction here
+//ANSWER: The function getLastDigit does not receive any parameters, so it will always return the last digit of the variable 'num', which is 3.
 
-const num = 103;
+// const num = 103;
 
-function getLastDigit() {
-  return num.toString().slice(-1);
+//function getLastDigit() {
+//   return num.toString().slice(-1);
+// }
+
+//console.log(`The last digit of 42 is ${getLastDigit(42)}`);
+//console.log(`The last digit of 105 is ${getLastDigit(105)}`);
+//console.log(`The last digit of 806 is ${getLastDigit(806)}`);
+
+// Now run the code and compare the output to your prediction
+// =============> write the output here
+// Explain why the output is the way it is
+//ANSWER:
+// The last digit of 42 is 3
+// The last digit of 105 is 3
+// The last digit of 806 is 3
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here:
+
+function getLastDigit(number) {
+  return number.toString().slice(-1);
 }
 
 console.log(`The last digit of 42 is ${getLastDigit(42)}`);
 console.log(`The last digit of 105 is ${getLastDigit(105)}`);
 console.log(`The last digit of 806 is ${getLastDigit(806)}`);
-
-// Now run the code and compare the output to your prediction
-// =============> write the output here
-// Explain why the output is the way it is
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-// =============> write your new code here
 
 // This program should tell the user the last digit of each number.
 // Explain why getLastDigit is not working properly - correct the problem
