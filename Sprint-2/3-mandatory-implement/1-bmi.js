5077ea9dbab71e5dbd4b5e1440a05abc262d84e4
Laura <laurabejaranoa22@gmail.com>
2025-12-16 14:31:16 +0100

    answer 1-bmi.js

--- Sprint-2/3-mandatory-implement/1-bmi.js
+++ Sprint-2/3-mandatory-implement/1-bmi.js
@@ -15,5 +15,13 @@
 // It should return their Body Mass Index to 1 decimal place
 
 function calculateBMI(weight, height) {
+    const BMI= weight / (height * height);
+    return BMI.toFixed(1);
+
     // return the BMI of someone based off their weight and height
 }
+
+const weight = 54;
+const height =1.60;
+
+console.log(calculateBMI(weight,height));
