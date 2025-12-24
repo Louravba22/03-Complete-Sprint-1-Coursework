84194d15fab64363cd4dcfc7d510429a129e7755
Laura <laurabejaranoa22@gmail.com>
2025-12-24 11:39:39 +0100

    card-value-test.js

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,33 @@
 const getCardValue = require("./3-get-card-value");
 
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
+  const aceofSpades = getCardValue("A♠");
+  expect(aceofSpades).toEqual(11);
+});
 
 // Case 2: Handle Number Cards (2-10):
+
+test("should return numeric value for number cards (2-10)", () => {
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("10♦")).toEqual(10);
+});
+
 // Case 3: Handle Face Cards (J, Q, K):
+
+test("should return 10 for face cards (J, Q, K)", () => {
+  expect(getCardValue("J♣")).toEqual(10);
+  expect(getCardValue("Q♠")).toEqual(10);
+  expect(getCardValue("K♥")).toEqual(10);
+});
+
 // Case 4: Handle Ace (A):
+
+test("should return 11 for any Ace", () => {
+  expect(getCardValue("A♦")).toEqual(11);
+});
+
 // Case 5: Handle Invalid Cards:
+
+test("should throw an error for invalid cards", () => {
+  expect(() => getCardValue("X♠")).toThrow("Invalid card rank");
+});
